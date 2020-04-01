80f3f4a14ed939756f2814449e0d83c4fe438008
kodamanbou <kodamanbou0424@gmail.com>
2020-04-01 10:51:05 +0900

    Add preprocessing.

--- preprocess2.py
+++ preprocess2.py
@@ -5,44 +5,6 @@ from utils import *
 from hyperparams import Hyperparameter as hp
 
 
-def process1(folder):
-    """
-    Trim the silence.
-    :param folder: folder of each speakers.
-    :return:
-    """
-    divs = 64
-    X = []
-    for file in glob.glob(folder + '/*.wav'):
-        wav, _ = librosa.load(file, sr=hp.rate, mono=True)
-        wav *= 1. / max(0.01, np.max(np.abs(wav)))
-
-        wav_splitted = librosa.effects.split(wav, top_db=48)
-
-        export_dir = folder.split('/')[1]
-        os.makedirs(os.path.join(hp.data_processed, export_dir), exist_ok=True)
-
-        for s in range(wav_splitted.shape[0]):
-            x = wav[wav_splitted[s][0]:wav_splitted[s][1]]
-            X = np.concatenate([X, x], axis=0)
-    X *= 1. / max(0.01, np.max(np.abs(X)))
-    wavlen = X.shape[0]
-    crop_size = wavlen // divs
-    start = 0
-    for i in range(divs):
-        sub = 0
-        if i == divs - 1:
-            sub = X[start:]
-        else:
-            sub = X[start:start + crop_size]
-
-        start += crop_size
-        sub = sub.astype(np.float32)
-        librosa.output.write_wav(
-            os.path.join(hp.data_processed, export_dir, "{}_".format(i) + os.path.basename(folder) + ".wav"), sub,
-            hp.rate)
-
-
 def process2(folder):
     train_A_dir = os.path.join(hp.data_processed, folder)
     exp_dir = os.path.join('pickles', folder)
@@ -87,19 +49,11 @@ def process2(folder):
 
 
 if __name__ == '__main__':
-    folders = glob.glob(hp.data_src + '/*')
-    print('Preprocess 1...')
-    TIME = time.time()
-    cores = min(len(folders), 4)
-    p = Pool(cores)
-    p.map(process1, folders)
-    p.close()
-    print(f'Preprocess 1 done in {time.time() - TIME} t.')
-
     print('Preprocess 2...')
     folders = os.listdir(hp.data_processed)
+    cores = min(len(folders), 4)
     TIME = time.time()
-    p2 = Pool(cores)
-    p2.map(process2, folders)
-    p2.close()
+    p = Pool(cores)
+    p.map(process2, folders)
+    p.close()
     print(f'Preprocess 2 done in {time.time() - TIME} t.')

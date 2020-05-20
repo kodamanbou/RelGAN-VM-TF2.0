dae120eb26624e11752bea8667092c071cf7a4c4
kodamanbou <kodamanbou0424@gmail.com>
2020-05-20 21:11:50 +0900

    Add demo.

---
+++ demo.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+import os
+from model import RelGAN
+from hyperparams import Hyperparameter as hp
+from utils import *
+import pyaudio
+import threading
+
+
+def worker(flag, lock):
+    key = str(input('Press q to quit.'))
+    while key != 'q':
+        key = str(input('Press q to quit.'))
+
+    lock.acquire()
+    flag['recording'] = False
+    lock.release()
+
+
+if __name__ == '__main__':
+    p = pyaudio.PyAudio()
+    chunk = 1024
+    stream = p.open(format=pyaudio.paInt16,
+                    channels=1,
+                    rate=44100,
+                    input=True,
+                    output=True,
+                    frames_per_buffer=chunk)
+
+    flag = {'recording': True}
+    lock = threading.Lock()
+    t1 = threading.Thread(target=worker, args=(flag, lock))
+    t1.start()
+
+    while flag['recording']:
+        data = stream.read(chunk)
+        output = stream.write(data)
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
